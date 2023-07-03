37e59e555ffa9bef53ee3f52a73f1bf740518686
Maricarmen <ecoaquiracu@unsa.edu.pe>
2023-07-02 23:09:08 -0500

    Mqtt handler

--- app/src/main/java/com/example/projectdanp/MainActivity.kt
+++ app/src/main/java/com/example/projectdanp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projectdanp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,13 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.projectdanp.graphs.RootNavigationGraph
 import com.example.projectdanp.ui.theme.NestedNavigationBottomBarDemoTheme
+import com.example.projectdanp.MqttHandler
 
 class MainActivity : ComponentActivity() {
+    private val BROKER_URL = "ssl://a14yncwleucddp-ats.iot.us-east-2.amazonaws.com:8883"
+    private val CLIENT_ID = "237065139024"
+    private var mqttHandler = MqttHandler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,8 +27,28 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     RootNavigationGraph(navController = rememberNavController())
+                    //mqttHandler = MqttHandler()
+                    mqttHandler.connect(BROKER_URL, CLIENT_ID)
+                    //subscribeToTopic("esp8266/sub")
+                    publishMessage("esp8266/sub", "Holaaa")
+
                 }
             }
         }
     }
+
+    override fun onDestroy() {
+        mqttHandler.disconnect()
+        super.onDestroy()
+    }
+
+    fun publishMessage(topic: String, message:String){
+        Toast.makeText(this,"Publising message: $message", Toast.LENGTH_LONG).show()
+        mqttHandler.publish(topic, message)
+    }
+    fun subscribeToTopic(topic: String){
+        Toast.makeText(this,"Subscribing to topic: $topic", Toast.LENGTH_LONG).show()
+        mqttHandler.subscribe(topic)
+    }
+
 }
